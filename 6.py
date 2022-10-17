9a86aecaade760b6255a90848f59907627c840b4
Code4ever10 <115994891+Code4ever10@users.noreply.github.com>
2022-10-17 17:37:37 +0530

    Update 6.py
    Comment

--- 6.py
+++ 6.py
@@ -1,9 +1,9 @@
-#6.SOLID BACKGROUND
+#SOLID BACKGROUND
 #CREATE WHITE OR BLACK BACKGROUND
 import numpy as np
 import cv2
 
 img = np.ones((500,500,3))
-cv2.imshow('White background',img)
+cv2.imshow('White background',img) #read image
 cv2.waitKey(0)
 cv2.destroyAllWindows()
